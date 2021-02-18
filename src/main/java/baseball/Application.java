fd00426cc4457134a5396e60b6a6eace677b10bf
Lee Ji Heok <olzlgur@naver.com>
2021-02-18 16:33:18 +0900

    restart function add

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,7 +8,13 @@ public class Application {
         GamePlayer gamePlayer = new GamePlayer();
 
         //게임 시작
+        while(true){
         gamePlayer.play(scanner);
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        if(scanner.nextInt()==1)
+            continue;
+        break;
+        }
         
     }
 }
